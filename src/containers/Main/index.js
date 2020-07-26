6c0424febca5b5740a99d036ead8adf624946cd6
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-07-26 08:21:11 -0300

    add: header basic style

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -5,7 +5,33 @@ import Animation from 'components/Animation'
 function Main(props) {
   return (
     <StyledMain>
-      <Animation />
+      <header>
+        <div className="col">
+          <div className="card">
+            <h1>Covid 19 Reports</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas eum earum commodi, quae omnis magnam. Suscipit, numquam quae officia expedita in sed consequatur quia maxime reprehenderit exercitationem, saepe aut recusandae!</p>
+            <a className="button" href="#">See reports</a>
+          </div>
+        </div>
+        <div className="col">
+          <Animation />
+        </div>
+      </header>
+      <main>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab laudantium doloremque dolorem, ea maiores aliquam soluta aperiam! Ducimus, excepturi? Voluptas ullam alias consectetur temporibus tempora eos possimus voluptate deleniti quis!
+      </main>
     </StyledMain>
   )
 }
--- src/containers/Main/style.js
+++ src/containers/Main/style.js
@@ -1,11 +1,46 @@
 import styled from 'styled-components'
 import tw from 'twin.macro'
 
+const background_color = 'bg-green-800'
+
 const StyledMain = styled.main.attrs({
   className: ""
 })`
   & {
+    height: 100vh;
 
+    header {
+      ${tw`${background_color} overflow-hidden h-full grid lg:grid-cols-2`}
+    }
+
+    .col {
+      ${tw`flex items-center justify-center`}
+    }
+
+    .card {
+      z-index: 1;
+      ${tw`bg-green-700 rounded shadow-lg p-8 m-8`}
+    }
+
+    h1{
+      ${tw`text-4xl font-bold`}
+    }
+
+    .animation {
+      z-index: 0;
+      transform: scale(-2) rotateX(180deg);
+      * {
+        ${tw`select-none cursor-default`}
+      }
+    }
+
+    p {
+      ${tw`text-xl my-4`}
+    }
+
+    .button {
+      ${tw`inline-block uppercase rounded-lg font-bold shadow-lg hover:shadow-xs p-4`}
+    }
   }
 `
 
