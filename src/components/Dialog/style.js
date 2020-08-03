52de919738fb23c7914c9a07e557d682c5129e0f
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-08-03 02:24:41 -0300

    add: dialog translate transition

--- src/components/Dialog/style.js
+++ src/components/Dialog/style.js
@@ -19,8 +19,16 @@ const StyledDialog = styled.div.attrs({})`
         ${tw`border-4 font-semibold px-4 rounded-full uppercase hover:border-red-400`}
       }
       
+
+      .dialog-backdrop {
+        will-change: transform;
+        transform: translateY(100%);
+        transition: transform .3s ease-out;
+      }
+
       .dialog-backdrop.active {
         ${tw`bg-black fixed h-full left-0 overflow-y-auto p-8 top-0 z-50 w-full`}
+         transform: translateY(0);
       }
   }
 `
