fbaa71743fd940b05c49ed424025fb6554137d27
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-08-04 21:48:07 -0300

    refactor: app countries data css and code improves

--- src/containers/App/AppCountriesDataSection/index.js
+++ src/containers/App/AppCountriesDataSection/index.js
@@ -2,46 +2,44 @@ import React from 'react'
 import StyledAppCountriesDataSection from './style'
 import NewsRow from 'components/NewsRow'
 import ReportsRow from 'components/ReportsRow'
-import countries from 'commons/constants/countries'
 
 const AppCountriesDataSection = ({ country }) => {
   const getDate = () => {
-    let dd, mm, yyyy, _ = new Date()
+    let dd, mm, yyyy, newDate = new Date()
 
-    dd = _.getDate()
-    mm = _.getMonth()
-    yyyy = _.getFullYear()
+    dd = newDate.getDate()
+    mm = newDate.getMonth()
+    yyyy = newDate.getFullYear()
 
     return `${dd}/${mm}/${yyyy}`
   }
 
+  const countryFlagSrc = `https://www.countryflags.io/${country.code}/flat/64.png`
+  const countryFlagAlt = `${country.name}'s flag`
+
   return (
     <StyledAppCountriesDataSection>
 
-      <section id="reports">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap justify-between items-center">
-            <div className="flex items-center">
-              <img src={`https://www.countryflags.io/${country.code}/flat/64.png`}></img>
-              <h1 className="text-3xl font-serif  ml-8">Reports</h1>
-            </div>
-            <div className="flex items-center">
-              <div className="font-thin text-2xl font-serif">Last data</div>
-            </div>
+      <section id="reports" className="reports-section">
+        <div className="reports-section__header">
+          <div className="reports-section__header-element">
+            <img className="reports-section__flag" src={countryFlagSrc} alt={countryFlagAlt} />
+            <h1 className="reports-section__title">Reports</h1>
+          </div>
+          <div className="reports-section__header-element">
+            <div className="reports-section__title">Last data</div>
           </div>
         </div>
         <ReportsRow place={country} />
-        <ReportsRow place={{name: "World", code: "earth" }} />
+        <ReportsRow place={{ name: "World", code: "earth" }} />
       </section>
 
-      <section id="news">
-        <div className="container mx-auto mt-8 px-4">
-          <div className="flex flex-wrap justify-start items-center">
-            <div className="flex items-center">
-              <div className="font-thin text-2xl font-serif">Today news</div>
-              <div className="h-4 w-4 mx-4 rounded-full bg-green-400"></div>
-              <time className="font-thin text-2xl font-serif" datatime={Date.now()}>{getDate(Date.now())}</time>
-            </div>
+      <section id="news" className="news-section">
+        <div className="news-section__header">
+          <div className="news-section__header-element">
+            <div className="news-section__title">Today news</div>
+            <div className="news-section__dot"></div>
+            <time className="news-section__title" datatime={Date.now()}>{getDate(Date.now())}</time>
           </div>
         </div>
         <NewsRow country={country} />
--- src/containers/App/AppCountriesDataSection/style.js
+++ src/containers/App/AppCountriesDataSection/style.js
@@ -6,6 +6,33 @@ const StyledAppCountriesDataSection = styled.section.attrs({
 })`
   & {
     
+    .reports-section__header,
+    .news-section__header {
+      ${tw`container mx-auto px-4 flex flex-wrap justify-between items-center`}
+    }
+
+    .reports-section__header-element,
+    .news-section__header-element {
+      ${tw`flex items-center`}
+    }
+    
+    .reports-section__flag {
+      ${tw`mr-8`}
+    }
+
+    .news-section__header {
+      ${tw`mt-8`}
+    }
+
+    .news-section__dot {
+      ${tw`h-4 w-4 mx-4 rounded-full bg-green-400`}
+    }
+
+    .reports-section__title,
+    .news-section__title {
+      ${tw`font-bold text-3xl font-serif`}
+    }
+
     .news-row,
     .reports-row {
       scrollbar-width: none; /* Firefox */
