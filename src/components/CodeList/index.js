21b1ccf0560c00a09973c34e38d4da8338be654f
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-08-04 22:05:07 -0300

    refactor: Code list css BEM classes and code improves

--- src/components/CodeList/index.js
+++ src/components/CodeList/index.js
@@ -3,18 +3,23 @@ import StyledCodeList from './style'
 import countries from 'commons/constants/countries'
 
 const CodeList = (props) => {
+  const CodeListItem = ({ country: { name, code } }) => {
+    return (
+      <div key={code} className="code-list__item">
+        <div className="code-list__item-name">{name}</div>
+        <div className="code-list__item-code">{code}</div>
+      </div>
+    )
+  }
+
+  const CodeListItems = () => {
+    return Object.values(countries)
+      .map((country, index) => <CodeListItem key={index} country={country} />)
+  }
+
   return (
     <StyledCodeList>
-
-      <div className="grid grid-cols-2 lg:grid-cols-3">
-        {Object.values(countries).map(country => (
-          <div key={country.code} className="flex flex-col p-4">
-            <div className="text-2xl">{country.name}</div>
-            <div className="border-b-8 border-blue-400 inline font-bold uppercase text-4xl w-1/6">{country.code}</div>
-          </div>
-        ))}
-      </div>
-
+      <CodeListItems />
     </StyledCodeList>
   )
 }
--- src/components/CodeList/style.js
+++ src/components/CodeList/style.js
@@ -3,7 +3,21 @@ import tw from 'twin.macro'
 
 const StyledCodeList = styled.div.attrs({
   id: "code-list",
-  className: ""
-})``
+  className: "code-list"
+})`
+  & {
+    ${tw`grid grid-cols-2 lg:grid-cols-3`}
+
+    .code-list__item {
+      ${tw`flex flex-col p-4`}
+    }
+    .code-list__item-name {
+      ${tw`text-2xl`}
+    }
+    .code-list__item-code {
+      ${tw`border-b-8 border-blue-400 inline font-bold uppercase text-4xl w-1/6`}
+    }
+  }
+`
 
 export default StyledCodeList
